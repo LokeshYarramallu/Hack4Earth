d98d0ba982f27d382ef7412571f6921dadfe4fa5
Madan2248c <madangopalboddu123@gmail.com>
2023-09-23 15:17:15 +0530

    login implemented in server and client side

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
-const productRoute = require('./routes/products.route');
-const sellersroute = require('./routes/sellerprofile.route');
-const customersroute = require('./routes/customer.route');
-app.set('view engine', 'ejs');
+const productRoute = require("./routes/products.route");
+const sellersroute = require("./routes/sellerprofile.route");
+const customersroute = require("./routes/customer.route");
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/products', productRoute);
-app.use('/sellers', sellersroute);
-app.use('/customers', customersroute);
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/signin_signup.html')
-})
-
-app.listen(3000, () => {
-    console.log('Server is running at : ' + `http://localhost:3000`);
+app.use("/products", productRoute);
+app.use("/sellers", sellersroute);
+app.use("/customers", customersroute);
+app.get("/signin", (req, res) => {
+  res.sendFile(__dirname + "/views/signin_signup.html");
+});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.listen(3000, () => {
+  console.log("Server is running at : " + `http://localhost:3000`);
+});
